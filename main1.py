e1eac07e8bbb4546a6ccd9bb812d9f3b529e89c7
GamerDog-HUN <nagyvathydavid@gmail.com>
2023-02-08 08:45:56 +0100

    main1.py elkészült

---
+++ main1.py
@@ -0,0 +1,25 @@
+#0
+print("Tanulok feldolgozása.")
+
+#1
+tanulok = []
+while True:
+    print("Kérem a tanuló adatait:")
+    nev = input("Tanulo neve: ")
+    szId = input("Tanuló születési ideje: ")
+    magassag = int(input("Magasság: "))
+
+    tanulo =(nev, szId, magassag)
+    tanulok.append(tanulo)
+
+    valasz = input("További tanuló? (igen/nem): ")
+    if valasz.lower() != 'igen':
+        break
+
+    #2
+
+
+    #3 Hozzáférés listaelem segítségével.
+print("\nTanulók listája\n")
+for item in tanulok:
+    print(f"Név: {item[0]}, születési ideje: {item[1]}, nagasság: {item[2]}")
